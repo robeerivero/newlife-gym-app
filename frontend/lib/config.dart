bc8a51518c42395ed89f03dc433d4e57f9c96aa4
robeerivero <roberto.riverodiaz@alum.uca.es>
2025-06-30 21:42:55 +0200

    version final

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  //static const String baseUrl = "https://newlife-gym-app.onrender.com";
-  static const String baseUrl = "http://192.168.0.26:5000";
+  static const String baseUrl = "https://newlife-gym-app.onrender.com";
+  //static const String baseUrl = "http://192.168.0.26:5000";
 }
